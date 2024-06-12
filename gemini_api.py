dbdc54e84773370892218c4d5cb503661737c3b4
wallacesintra <wallvxwallace962@gmail.com>
2024-06-12 22:30:36 +0300

    feat: Refactor MyGeminiApi class to improve code organization and readability

--- gemini_api.py
+++ gemini_api.py
@@ -2,8 +2,8 @@
 
 """
 Class MyGeminiApi is responsible for the gemini api
-
 """
+
 import google.generativeai as genai
 
 from writePrompt import write_file
@@ -11,19 +11,22 @@ from util import Util
 
 genai.configure(api_key= "your_gemini_api_key")
 
-
 util = Util()
 
-
-
 class MyGeminiApi():
+    """
+    Class MyGeminiApi is responsible for the gemini api
+    """
+
     def __init__(self):
         self.generativeModel = genai.GenerativeModel('gemini-pro')
 
     def generate(self, text):
         response = self.generativeModel.generate_content(text)
 
-        title = self.generativeModel.generate_content("generate a title and the title should not be more than 5 words in text for " + response.text)
+        title = self.generativeModel.generate_content(
+            "generate a title and the title should not be more than 5 words in text for " + response.text
+            )
 
         promptTitle = util.replace_whitespace_with_underscore(title.text)
 
@@ -35,4 +38,3 @@ class MyGeminiApi():
         responseText = response.text
 
         return responseText
-    
--- util.py
+++ util.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 class Util():
+
+    """
+    Class Util is responsible for utility functions
+    """
     def __init__(self):
         pass
 
     def write_file(self, file_name, content):
+        """
+        Write content to a file
+        """
         import os
 
         if not os.path.exists('history'):
@@ -18,6 +25,9 @@ class Util():
             return True
 
     def read_file(self, file_name):
+        """
+        Read content from a file
+        """
         with open(file_name, 'r') as file:
             content = file.read()
             file.close()
@@ -25,12 +35,18 @@ class Util():
 
     
     def get_file_list(self):
+        """
+        Get list of files in the history directory
+        """
         import os
 
         file_list = os.listdir('history')
         return file_list
 
     def delete_file(self, file_name):
+        """
+        Delete a file
+        """
         import os
 
         file_name = os.path.join('history', file_name)
@@ -38,12 +54,18 @@ class Util():
         return True
     
     def clear_screen(self):
+        """
+        Clear the screen
+        """
         import os
 
         os.system('clear')
         return True
 
     def clear_all_history(self):
+        """
+        Clear all history
+        """
         import os
 
         if(os.path.exists('history')):
@@ -52,13 +74,22 @@ class Util():
 
 
     def get_file_path(self, file_name):
+        """
+        Get the file path
+        """
         import os
 
         file_name = os.path.join('history', file_name)
         return file_name
     
     def replace_whitespace_with_underscore(self, text):
+        """
+        Replace whitespace with underscore
+        """
         return text.replace(" ", "_")
     
     def replace_underscore_with_whitespace(self, text):
+        """
+        Replace underscore with whitespace
+        """
         return text.replace("_", " ")
